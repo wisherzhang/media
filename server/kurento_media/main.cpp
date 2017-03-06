0701ce195122bdd4c5919d31eb2038258a3b0529
659539109@qq.com <659539109@qq.com>
2017-03-06 19:13:58 +0800

    add death_handler.cpp

--- server/kurento_media/main.cpp
+++ server/kurento_media/main.cpp
@@ -1,8 +1,11 @@
 
-
+#include "death_handler.hpp"
 int main(int argc,char*argv[])
 {
-
-
+        Debug::DeathHandler dh;
+        dh.set_thread_safe (true);
+        dh.set_color_output (true);
+        int *p = NULL;
+        *p = 10;
 	return 0;
 }
